637de632b0c2d0a35353363041f8dc8833013749
Raspberry Pi User <pi@raspberrypi.(none)>
2012-07-28 16:23:18 +0100

    it works

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import serial
 
+import serial
 
 
 
@@ -11,13 +11,40 @@ class Rfid:
 
 		self.ser = serial.Serial( self.dev_path , 2400 )
 
-	def main(self):
+	def listen(self):
+		last = None
 		while True:
-			print self.ser.readline() 
+			v = self.ser.readline()
+			if v != last:
+				last = v
+				self.onread(v.rstrip())
+
+	def onread(self,data):
+		print data
+
+
+
+
+
+def custom_onread(data):
+	hashs = {
+		'010A38FAAD' : 'Edog',
+		'4D004A56E2' : 'Shiggins'
+	}
+	try:
+		name = hashs[data]
+	except KeyError:
+		name = None
+
+	print name
+
+
 
 def main():
 	rfid = Rfid()
-	rfid.main()
+	rfid.onread = custom_onread
+	rfid.listen()
 
 if __name__ == '__main__':
 	main();
+
