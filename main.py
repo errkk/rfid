02e0fc411e38553b60da7d4a269383b9fb22b720
Eric <errkkgeorge+github@gmail.com>
2012-07-28 14:50:53 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,21 @@
+import serial
+
+# connect port
+ser = serial.Serial('/dev/ttyUSB0', 2400)
+
+
+class Rfid:
+	dev_path = '/dev/ttyUSB0'
+
+	def __init__(self):
+
+		self.serial.Serial( self.dev_path , 2400 )
+
+	def itterator(self):
+		while 1:
+			yield self.serial.readline()
+
+
+	def main(self):
+		for i in self.itterator():
+			print i 
